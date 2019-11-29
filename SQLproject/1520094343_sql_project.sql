13d448bd3f8a42983e5cac1038a841d91e9e48e5
atalebizadeh <alireza.talebizadeh@gmail.com>
2019-11-29 16:39:12 -0600

    Update 1520094343_sql_project.sql

--- SQLproject/1520094343_sql_project.sql
+++ SQLproject/1520094343_sql_project.sql
@@ -1,10 +1,10 @@
-/* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
+/* Welcome to the subqL mini project. For this project, you will use
+Springboard' online subqL platform, which you can log into through the
 following link:
 
-https://sql.springboard.com/
+https://subql.springboard.com/
 Username: student
-Password: learn_sql@springboard
+Password: learn_subql@springboard
 
 The data you need is in the "country_club" database. This database
 contains 3 tables:
@@ -30,14 +30,13 @@ Please list the names of the facilities that do. */
 SELECT name, membercost
 FROM Facilities
 WHERE membercost > 0
---ORDER BY membercost DESC
 
 
 /* Q2: How many facilities do not charge a fee to members? */
 
 SELECT COUNT( * )
 FROM Facilities
-WHERE membercost =0
+WHERE membercost = 0
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
@@ -45,14 +44,14 @@ where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
-SELECT  facid, 
-	 	name,
+SELECT	facid, 
+		name,
 		membercost,
 		monthlymaintenance
 FROM Facilities
 WHERE membercost > 0
 AND membercost < (.20 * monthlymaintenance)
---ORDER BY membercost DESC
+
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
@@ -67,14 +66,13 @@ WHERE facid IN (1,5)
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
 
-SELECT 	name,
+SELECT	name,
 		monthlymaintenance,
 CASE 
 	WHEN monthlymaintenance > 100 THEN "Expensive"
 	ELSE "Cheap"
 END AS Pricing_Group
 FROM Facilities
---ORDER BY monthlymaintenance DESC
 
 
 /* Q6: You'd like to get the first and last name of the last member(s)
@@ -85,7 +83,6 @@ SELECT  firstname,
 		joindate
 FROM Members
 WHERE joindate = (SELECT MAX(joindate) FROM Members)
---ORDER BY joindate DESC
 
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
@@ -93,13 +90,13 @@ Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
-SELECT DISTINCT f.name AS Court_names,
+SELECT DISTINCT	f.name AS Facility_names,
 				CONCAT(m.firstname,' ',m.surname) AS Member_names
 	FROM Bookings b
 	JOIN Members m ON b.memid = m.memid
-	AND m.memid > 0
+	AND m.memid > 0 -- To select all members only
 	JOIN Facilities f ON b.facid = f.facid	
-WHERE b.facid < 2
+WHERE b.facid < 2 -- To select tennis courts
 ORDER BY 2
 
 
@@ -113,12 +110,12 @@ Order by descending cost, and do not use any subqueries. */
 SELECT  f.name as facility_name,
 		CONCAT(m.firstname,' ',m.surname) AS Member_names,
 CASE
-	WHEN b.memid = 0 THEN (b.slots * f.guestcost)
-	ELSE (b.slots * f.membercost)
+	WHEN b.memid = 0 THEN (b.slots * f.guestcost) -- To calculate the total cost for guests
+	ELSE (b.slots * f.membercost) -- To calculate the total cost for members (if memid is not 0, then they are members)
 END AS cost
 FROM Bookings b
 JOIN Facilities f ON b.facid = f.facid
-	 AND b.starttime LIKE '2012-09-14%'
+	 AND b.starttime LIKE '2012-09-14%' -- To select the day of 2012-09-14
 JOIN Members m ON b.memid = m.memid
 	WHERE 	(CASE 
 				WHEN b.memid = 0 THEN (b.slots * f.guestcost)
@@ -127,12 +124,11 @@ JOIN Members m ON b.memid = m.memid
 ORDER BY 3 DESC
 
 
-
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
-SELECT  sq.facility_name,
+SELECT  subq.facility_name,
 		CONCAT(m.firstname,' ',m.surname) AS Member_names,
-		sq.cost
+		subq.cost
 FROM Members m
 JOIN (SELECT f.name as facility_name,
 			b.memid as memid,
@@ -142,8 +138,8 @@ JOIN (SELECT f.name as facility_name,
 			END AS cost		
 	FROM Bookings b
 	JOIN Facilities f ON b.facid = f.facid
-	AND b.starttime LIKE '2012-09-14%') sq	ON m.memid = sq.memid
-WHERE sq.cost > 30
+	AND b.starttime LIKE '2012-09-14%') subq	ON m.memid = subq.memid
+WHERE subq.cost > 30
 ORDER BY 3 DESC
 
 
@@ -152,13 +148,13 @@ The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
 
 SELECT f.name AS facility_name,
-       sq1.total_revenue
+       subq1.total_revenue
 FROM Facilities f
 JOIN (
     SELECT f.facid as facid,
 			sum(CASE
-					WHEN sq2.user_type = 'Guest' THEN sq2.total_slots * f.guestcost
-                    ELSE sq2.total_slots * f.membercost
+					WHEN subq2.user_type = 'Guest' THEN subq2.total_slots * f.guestcost
+                    ELSE subq2.total_slots * f.membercost
 				END) AS total_revenue
     FROM Facilities f
     JOIN (
@@ -171,8 +167,8 @@ JOIN (
         FROM Bookings b
         JOIN Facilities f ON b.facid = f.facid
         GROUP BY facid, user_type
-    ) sq2 ON f.facid = sq2.facid
+    ) subq2 ON f.facid = subq2.facid
     GROUP BY 1
-) sq1 ON f.facid = sq1.facid
-WHERE sq1.total_revenue < 1000
+) subq1 ON f.facid = subq1.facid
+WHERE subq1.total_revenue < 1000
 ORDER BY 2
